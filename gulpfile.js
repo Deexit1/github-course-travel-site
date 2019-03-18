737ae187eb648ed54178c02cd08038ed8cc886c9
deexit <deexitpatel4@gmail.com>
2019-03-18 19:10:14 +0530

    completed errora

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,5 @@ require('./gulp/tasks/styles');
 require('./gulp/tasks/watch');
 require('./gulp/tasks/sprites');
 require('./gulp/tasks/scripts');
-<<<<<<< HEAD
-require('./gulp/tasks/modenizr');
-require('./gulp/tasks/build');
-=======
 require('./gulp/tasks/build');
 require('./gulp/tasks/modenizr');
->>>>>>> build
